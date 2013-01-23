e63d6e076dc66cebe5e528586d88fba4ffe37e5e
Josh Ferrara <josh@hivemindlabs.com>
2013-01-22 23:12:15 -0600

    recalculate vertical spacing on page resize

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -1,16 +1,25 @@
 jQuery(document).ready(function($){
-		
+
 	// page load inbootstrap
 	offset = ((window.innerHeight - 510) / 2);
 	if (offset <= 25){
-		offset = 25 
+		offset = 25
 	};
 	$('body.home .logo').css('margin-top', offset);
 
-	
+  $(window).resize(function(){
+  	offset = ((window.innerHeight - 510) / 2);
+  	if (offset <= 25){
+  		offset = 25
+  	};
+  	$('body.home .logo').css('margin-top', offset);
+  });
+
+
+
 	// dim icons
 	$('.icon').css('opacity', '0.2');
-	
+
 	// fade in/out icons on hover
 	$('.icon').hover(function(){
 		$(this).stop().animate({opacity: '1'}, 200);
@@ -18,7 +27,7 @@ jQuery(document).ready(function($){
 	}, function(){
 		$(this).stop().animate({opacity: '0.2'}, 200);
 	});
-	
+
 	// icon tooltips
 	$('.icon a').tooltip({placement: 'bottom'});
 
@@ -33,7 +42,7 @@ jQuery(document).ready(function($){
 		$(this).removeClass('hover');
 		$('.bio_content').removeClass('hover').stop().fadeOut(200);
 	});
-	
-	
+
+
 });
 
