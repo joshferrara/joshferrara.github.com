7e3b26e738e70f3d716321a51f32669bb022bed4
Josh Ferrara <josh@hivemindlabs.com>
2013-11-10 22:27:41 -0600

    Update twitter archive

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details =  {
-  "tweets" : 4933,
-  "created_at" : "Sun Mar 03 22:33:26 +0000 2013",
+  "tweets" : 5406,
+  "created_at" : "2013-11-11 04:20:14 +0000",
   "lang" : "en"
 }
